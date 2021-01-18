3c078cf7c35046026bd29900675c7d30a30c2ee5
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-18 19:26:32 +0000

    updated urls_index.ejs and made a route for /urls

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -9,6 +10,11 @@ const urlDatabase = {
 
 app.get('/', (req, res) => {
   res.send('hello');
+});
+
+app.get('/urls', (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
 });
 
 app.get('/urls.json', (req, res) => {
