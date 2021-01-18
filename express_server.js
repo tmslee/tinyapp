09e98c683e1a2020b1ce8f18a7be6c0a9b14a508
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-18 20:57:40 +0000

    created urls_new.ejs, made a get request and installed bodyparser

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const PORT = 8080;
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,17 +19,13 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req,res) => {
+  res.render('urls_new');
+ });
+
 app.get('/urls/:shortURL',(req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
-});
-
-app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get('/hello', (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
