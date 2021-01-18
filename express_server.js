78604ae5023a0c2f172ea3c118cee8fd3a1d9b48
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-18 21:16:06 +0000

    wrote generateRandomString function

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -28,6 +28,21 @@ app.get('/urls/:shortURL',(req, res) => {
   res.render('urls_show', templateVars);
 });
 
+app.post('/urls', (req,res) => {
+  console.log(req.body);
+  res.send('ok');
+})
+
 app.listen(PORT, () => {
   console.log(`exaple app listening on port ${PORT}`);
 });
+
+//
+function generateRandomString(len){
+  let result = '';
+  const CHARS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+  for (let i = 0; i < len; i++){
+    result += CHARS[Math.floor(Math.random()*62)];
+  }
+  return result;
+}
