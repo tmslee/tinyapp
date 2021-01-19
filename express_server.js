2ac73c2194f38fdac24a28cef0c434fa37b214e6
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-19 22:48:22 +0000

    added login functionality using cookies

--- express_server.js
+++ express_server.js
@@ -1,18 +1,19 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT = 8080;
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 }
-
 ////////////////////////////////////////////
 ////////////////GET ROUTES ////////////////
 
@@ -21,16 +22,26 @@ app.get('/', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {
+    urls: urlDatabase, 
+    username: req.cookies['username']
+  };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req,res) => {
-  res.render('urls_new');
+  const templateVars = { 
+    username: req.cookies['username']
+  };
+  res.render('urls_new', templateVars);
  });
 
 app.get('/urls/:shortURL',(req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL], 
+    username: req.cookies['username']
+  };
   res.render('urls_show', templateVars);
 });
 
@@ -61,6 +72,16 @@ app.post('/urls/:id/edit', (req, res) => {
   res.redirect('/urls');
 });
 
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  res.cookie('username', username);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
 
 /////////////////////////////////////////////
 app.listen(PORT, () => {
